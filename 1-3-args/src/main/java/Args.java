46440cdf1a4f447598e2ef0e75758d9b95184662
CloneableX <1573652017@qq.com>
2019-09-04 22:43:30 +0800

    add command

---
+++ 1-3-args/src/main/java/Args.java
@@ -0,0 +1,13 @@
+package com.clo.tdd;
+
+/**
+ * com.clo.tdd.Args
+ *
+ * @author Cloneable
+ * @date 2019/9/4 22:42:04
+ * @description
+ */
+public class Args {
+    public Args(Schema schema) {
+    }
+}
---
+++ 1-3-args/src/main/java/Command.java
@@ -0,0 +1,13 @@
+package com.clo.tdd;
+
+/**
+ * com.clo.tdd.Command
+ *
+ * @author Cloneable
+ * @date 2019/9/4 22:43:04
+ * @description
+ */
+public class Command {
+    public Command(String command) {
+    }
+}
