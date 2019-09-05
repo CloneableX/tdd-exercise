e3d3e954e175fab4d642b6527586b56d4bf50bfa
CloneableX <1573652017@qq.com>
2019-09-05 23:05:19 +0800

    add command unit test

---
+++ 1-3-args/src/test/java/CommandTest.java
@@ -0,0 +1,17 @@
+package com.clo.tdd;
+
+import org.junit.Test;
+
+/**
+ * com.clo.tdd.CommandTest
+ *
+ * @author Cloneable
+ * @date 2019/9/5 23:04:05
+ * @description unit test for Command
+ */
+public class CommandTest {
+    @Test
+    public void should_get_args_size_when_give_command_string() {
+
+    }
+}
