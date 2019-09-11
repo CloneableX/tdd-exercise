04dc6f2625a178d548fb03cac9cbb14e2f136543
CloneableX <1573652017@qq.com>
2019-09-11 22:52:55 +0800

    create argument definition unit test

---
+++ 1-3-args/src/test/java/ArgDefTest.java
@@ -0,0 +1,17 @@
+package com.clo.tdd;
+
+import org.junit.Test;
+
+/**
+ * com.clo.tdd.ArgDefTest
+ *
+ * @author Cloneable
+ * @date 2019/9/11 22:51:11
+ * @description test unit for ArgDef
+ */
+public class ArgDefTest {
+    @Test
+    public void should_get_create_arg_def() {
+
+    }
+}
