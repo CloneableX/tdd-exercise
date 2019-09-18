fb0b85c16bf85716cb219bec9c3bdfa2549193c5
CloneableX <1573652017@qq.com>
2019-09-18 23:08:41 +0800

    change args assert

--- 1-4-args/src/test/java/com/clo/tdd/ArgsTest.java
+++ 1-4-args/src/test/java/com/clo/tdd/ArgsTest.java
@@ -1,20 +1,24 @@
 package com.clo.tdd;
 
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * com.clo.tdd.ArgsTest
  *
  * @author Cloneable
  * @date 2019/9/18 23:04:18
- * @description
+ * @description unit test for Args
  */
 public class ArgsTest {
     @Test
     @Ignore
     public void should_get_arg_value_when_give_arg_name() {
-        Args args = new Args("l:bool", "-l");
-        args.queryValue("l");
+        Args args = new Args("l:bool;p:int;d:string", "-l -p 8080 -d /usr/logs");
+        assertThat(args.queryValue("l"), is(Boolean.TRUE));
     }
 }
